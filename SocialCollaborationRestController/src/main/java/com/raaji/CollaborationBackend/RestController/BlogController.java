f46f85eaa11f32265a9a39fd5bce4377e0bb24ac
admin <admin@192.168.0.5>
2017-12-05 23:08:37 +0530

    This is my RestController Project...

--- SocialCollaborationRestController/src/main/java/com/raaji/CollaborationBackend/RestController/BlogController.java
+++ SocialCollaborationRestController/src/main/java/com/raaji/CollaborationBackend/RestController/BlogController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.raaji.CollaborationProject.DAO.BlogDAO;
-import com.raaji.CollaborationProject.Model.BlogPart;
+import com.raaji.CollaborationBackend.DAO.BlogDAO;
+import com.raaji.CollaborationBackend.Model.BlogPart;
+
 
 @RestController
 public class BlogController {
